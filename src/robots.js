06b4be8259a1e1fb37a22b4642f75af7e706f023
jnrade <junior.dejonge@gmail.com>
2022-06-20 14:12:32 -0400

    update robots.js

--- src/robots.js
+++ src/robots.js
@@ -2,51 +2,8 @@
 export const robots = [
     {
         id : 1,
-        name: 'Letelle De Jonge',
-        username: 'Letelle',
-        email: 'letelle@dejonge.com'
-    },
-    {
-        id : 2,
-        name: 'Virginia De Jonge',
-        username: 'Virginia',
-        email: 'virginia@dejonge.com'
-    },
-    {
-        id : 3,
-        name: 'Michael De Jonge',
-        username: 'Michael',
-        email: 'michael@dejonge.com'
-    },
-    {
-        id : 4,
-        name: 'Garvin De Jonge',
-        username: 'Garvin',
-        email: 'garvin@dejonge.com'
-    },
-    {
-        id : 5,
-        name: 'Leta De Jonge',
-        username: 'Leta',
-        email: 'leta@dejonge.com'
-    },
-    {
-        id : 6,
-        name: 'Charmaine De Jonge',
-        username: 'Charmaine',
-        email: 'charmaine@dejonge.com'
-    },
-    {
-        id : 7,
         name: 'Junior De Jonge',
         username: 'Junior',
         email: 'junior@dejonge.com'
-    },
-    {
-        id : 8,
-        name: 'Kwasi De Jonge',
-        username: 'Kwasi',
-        email: 'kwasi@dejonge.com'
     }
-
 ]
